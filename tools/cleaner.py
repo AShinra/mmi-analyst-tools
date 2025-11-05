1967c93ed8f47fc2a92015e399dcd4cb152636c8
Athan <jonpuray@gmail.com>
2025-11-05 13:46:41 +0800

    updated

--- tools/cleaner.py
+++ tools/cleaner.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import pandas as pd
 
 
 
 def cleaner():
-    ''''''
+    st.title('File Cleaner')
+
+    raw_file = st.file_uploader(
+        label='Input File',
+        label_visibility='hidden',
+        type=['xls', 'xlsx']
+    )
+
+    if raw_file:
+        df = pd.read_excel(raw_file)
+        st.dataframe(df)    
+    
